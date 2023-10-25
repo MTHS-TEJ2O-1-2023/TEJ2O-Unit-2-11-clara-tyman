23c6d3fd59cc83367048bdaeaa89f4354a3116dc
Clara <144264170+clara-tyman@users.noreply.github.com>
2023-10-25 12:29:35 -0400

    ts version

--- main.ts
+++ main.ts
@@ -2,7 +2,35 @@
  *
  * Created by: Mr. Coxall
  * Created on: Sep 2020
- * This program ...
+ * This program compares 2 random numbers from 0-99.
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// variables
+const firstNumber: number = randint(0, 99)
+const secondNumber: number = randint(0, 99)
+
+// show 1st number
+input.onButtonPressed(Button.A, function () {
+  basic.clearScreen()
+  basic.showString('#1 : ' + firstNumber.toString())
+})
+
+// show 2nd number
+input.onButtonPressed(Button.B, function () {
+  basic.clearScreen()
+  basic.showString('#2 : ' + secondNumber.toString())
+})
+
+// compare both numbers
+input.onGesture(Gesture.Shake, function () {
+  basic.clearScreen()
+  if (firstNumber < secondNumber) {
+    basic.showString(firstNumber.toString() + '<' + secondNumber.toString())
+    } else {
+        basic.showString(firstNumber.toString() + '>' + secondNumber.toString())
+    }
+    basic.showIcon(IconNames.Sad)
+})
